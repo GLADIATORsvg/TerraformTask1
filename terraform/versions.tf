a2d1c76f51b6b047be06c350108465182ee11726
Alexander Ovcharuk <147329862+GLADIATORsvg@users.noreply.github.com>
2025-12-08 23:09:34 +0200

    Update versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     github = {
       source  = "integrations/github"
-      version = "~> 5.0"
+      version = "~> 6.0"
     }
   }
-  required_version = ">= 1.5.0"
 }
 
 provider "github" {
   token = var.github_token
-  owner = var.repo_owner
+  owner = var.github_owner
 }
+

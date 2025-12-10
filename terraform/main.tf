b754848d6805c2a08473462f5812cfb0f903254f
Alexander Ovcharuk <147329862+GLADIATORsvg@users.noreply.github.com>
2025-12-10 19:08:27 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,8 @@
+### REPO NAME ###
+locals {
+  repo_name = var.repo_name
+}
+
 #### ---------------- COLLABORATOR ---------------- ####
 resource "github_repository_collaborator" "softserve" {
   repository = local.repo_name
@@ -7,71 +12,71 @@ resource "github_repository_collaborator" "softserve" {
 
 #### ---------------- DEVELOP BRANCH ---------------- ####
 resource "github_branch" "develop_branch" {
-  repository = local.repo_name
-  branch     = "develop"
+  repository     = local.repo_name
+  branch         = "develop"
+  source_branch  = "main"
 }
 
-resource "github_branch_default" "default_branch_default" {
+resource "github_branch_default" "default_branch" {
   repository = local.repo_name
   branch     = github_branch.develop_branch.branch
 }
 
 #### ---------------- BRANCH PROTECTIONS ---------------- ####
-resource "github_branch_protection" "develop_protec_rules" {
-  repository_id        = var.repo_name
-  pattern              = "develop"
-  
+resource "github_branch_protection" "develop_protection" {
+  repository_id = github_repository.repo.node_id
+  pattern       = "develop"
+
   required_pull_request_reviews {
     required_approving_review_count = 2
   }
+
+  enforce_admins = true
 }
 
-resource "github_branch_protection" "main_protect_rules" {
-  repository_id        = local.repo_name
-  pattern              = "main"
+resource "github_branch_protection" "main_protection" {
+  repository_id = github_repository.repo.node_id
+  pattern       = "main"
 
   required_pull_request_reviews {
     require_code_owner_reviews      = true
-    required_approving_review_count = 0
+    required_approving_review_count = 1
   }
+
+  enforce_admins = true
 }
 
 #### ---------------- PR TEMPLATE ---------------- ####
+resource "github_repository_file" "pr_template" {
+  repository          = local.repo_name
+  file                = ".github/pull_request_template.md"
+  content             = local.pr_tmplt_content
+  branch              = "main"
+  overwrite_on_create = true
+  commit_message      = "Add PR template"
+}
+
+#### ---------------- CODEOWNERS ---------------- ####
 resource "github_repository_file" "codeowners" {
   repository          = local.repo_name
   file                = ".github/CODEOWNERS"
   content             = "* @softservedata"
   branch              = "main"
   overwrite_on_create = true
-}
-
-resource "github_repository_file" "main_pr_template" {
-  repository          = local.repo_name
-  file                = ".github/pull_request_template.md"
-  content             = local.pr_tmplt_content
-  branch              = "main"
-  overwrite_on_create = true
-}
-
-resource "github_repository_file" "develop_pr_template" {
-  repository          = local.repo_name
-  file                = ".github/pull_request_template.md"
-  content             = local.pr_tmplt_content
-  branch              = "main"
-  overwrite_on_create = true
-  depends_on          = [github_branch.develop_branch]
+  commit_message      = "Add CODEOWNERS"
 }
 
 #### ---------------- DEPLOY KEY ---------------- ####
 resource "github_repository_deploy_key" "deploy_key" {
   title      = "DEPLOY_KEY"
-  repository = var.repo_name
-  key        = "ssh-rsa AAAAB3Nza..."
+  repository = local.repo_name
+  key        = var.deploy_key_public
+  read_only  = false
 }
 
 #### ---------------- ACTIONS SECRET ---------------- ####
 resource "github_actions_secret" "pat" {
-  repository      = var.repo_name
+  repository      = local.repo_name
   secret_name     = "PAT"
   plaintext_value = var.pat_value
 }

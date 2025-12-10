24ee8abf40cd68ab0e63fc4365a78ee3723a172a
Alexander Ovcharuk <147329862+GLADIATORsvg@users.noreply.github.com>
2025-12-10 19:23:17 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,16 @@
-### REPO NAME ###
 locals {
   repo_name = var.repo_name
   pr_tmplt_content  = var.pr_template_content
 }
 
-#### ---------------- COLLABORATOR ---------------- ####
+
 resource "github_repository_collaborator" "softserve" {
   repository = local.repo_name
   username   = "softservedata"
   permission = "push"
 }
 
-#### ---------------- DEVELOP BRANCH ---------------- ####
+
 resource "github_branch" "develop_branch" {
   repository     = local.repo_name
   branch         = "develop"
@@ -23,7 +22,7 @@ resource "github_branch_default" "default_branch" {
   branch     = github_branch.develop_branch.branch
 }
 
-#### ---------------- BRANCH PROTECTIONS ---------------- ####
+
 resource "github_branch_protection" "develop_protection" {
   repository_id = github_repository.repo.node_id
   pattern       = "develop"
@@ -47,7 +46,7 @@ resource "github_branch_protection" "main_protection" {
   enforce_admins = true
 }
 
-#### ---------------- PR TEMPLATE ---------------- ####
+
 resource "github_repository_file" "pr_template" {
   repository          = local.repo_name
   file                = ".github/pull_request_template.md"
@@ -57,7 +56,7 @@ resource "github_repository_file" "pr_template" {
   commit_message      = "Add PR template"
 }
 
-#### ---------------- CODEOWNERS ---------------- ####
+
 resource "github_repository_file" "codeowners" {
   repository          = local.repo_name
   file                = ".github/CODEOWNERS"
@@ -67,7 +66,7 @@ resource "github_repository_file" "codeowners" {
   commit_message      = "Add CODEOWNERS"
 }
 
-#### ---------------- DEPLOY KEY ---------------- ####
+
 resource "github_repository_deploy_key" "deploy_key" {
   title      = "DEPLOY_KEY"
   repository = local.repo_name
@@ -75,7 +74,7 @@ resource "github_repository_deploy_key" "deploy_key" {
   read_only  = false
 }
 
-#### ---------------- ACTIONS SECRET ---------------- ####
+
 resource "github_actions_secret" "pat" {
   repository      = local.repo_name
   secret_name     = "PAT"
